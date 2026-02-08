b54c3a04e65513fa7a8d7d1f97c9a5eeadb38c27
Charles Sena <charles.sena.sa@gmail.com>
2026-02-08 01:15:27 -0300

    feat: Response class tweak

--- index.php
+++ index.php
@@ -9,12 +9,18 @@ $request = Request::createFromGlobals();
 
 $name = $request->get('name','World');
 
+
+$response = new Response();
+
 $sprintfArgs = [
   'Hello %s',
   htmlspecialchars($name,ENT_QUOTES,'UTF-8')
 ];
 
-$response = new Response(sprintf(...$sprintfArgs));
+$response->setContent(sprintf(...$sprintfArgs));
+$response->setStatusCode(418);
+
+$response->setMaxAge(10);
 
 $response->send();
 
